2a3f94a67fc01e7fb3a2532cb3f1f0eb21d7a548
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-24 23:12:57 +0530

    navber menu added for mobile

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { LuMenu } from "react-icons/lu";
 import { BiSolidUser } from "react-icons/bi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import NavbarMenu from "./NavbarMenu";
 
 const Navbar = () => {
+	const [showMenu, setShowMenu] = useState(false);
+
 	return (
 		<div className='w-full p-6'>
 			<div className='bg-white w-full flex justify-between items-center p-4 md:px-6 rounded-xl'>
-				<div className='md:hidden w-1/3'>
-					<LuMenu size={25} />
-				</div>
+				<button className='md:hidden w-1/3'>
+					{!showMenu && (
+						<LuMenu onClick={() => setShowMenu(true)} size={25} className='cursor-pointer' />
+					)}
+					{showMenu && (
+						<>
+							<AiFillCloseCircle
+								size={25}
+								className='cursor-pointer'
+								onClick={() => setShowMenu(false)}
+							/>
+							<NavbarMenu />
+						</>
+					)}
+				</button>
 				<div className='hidden md:flex gap-6 w-1/3'>
 					<div>New Drops</div>
 					<div>Men</div>
---
+++ app/components/NavbarMenu.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavbarMenu = () => {
+	return (
+		<div className='absolute top-28 left-10 flex flex-col gap-4 p-4 bg-white rounded-lg items-start'>
+			<div>New Drops</div>
+			<div>Men</div>
+			<div>Women</div>
+		</div>
+	);
+};
+
+export default NavbarMenu;
