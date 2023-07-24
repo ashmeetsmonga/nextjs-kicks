78e194ea92d7d890b51ac1027abfa57f30556e2f
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-24 23:49:50 +0530

    added text on hero image

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col gap-4 md:gap-6'>
+		<main className='flex flex-col gap-4 md:gap-6'>
 			<div className='uppercase w-full text-center font-extrabold text-7xl md:text-9xl text-theme-dark-gray'>
 				Do it <span className='text-theme-blue'>right</span>
 			</div>
-			<div className='w-full h-96 md:h-[40rem] rounded-xl relative'>
+			<div className='w-full h-96 md:h-[40rem] rounded-xl relative bg-black '>
 				<Image
 					src={"/images/hero.png"}
-					className='rounded-xl'
+					className='rounded-xl vignette'
 					alt='hero'
 					layout='fill'
 					objectFit='cover'
 				/>
+				<div className='absolute bottom-0 left-0 text-white p-4 md:p-6 w-3/5'>
+					<h2 className='text-3xl md:text-6xl uppercase font-semibold'>Nike air max</h2>
+					<p className='md:text-xl'>Nike introducing the new air max for everyone's comfort</p>
+					<button className='px-4 py-2 mt-2 bg-theme-blue rounded-lg'>Shop now</button>
+				</div>
 			</div>
 		</main>
 	);
