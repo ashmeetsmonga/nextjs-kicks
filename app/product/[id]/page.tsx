ec72aa7324e10a16412f3858352d8b80449b80fd
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-26 17:56:30 +0530

    ui refactor changes

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -5,7 +5,7 @@ import ProductInfo from "./components/ProductInfo";
 const ProductDetails = () => {
 	return (
 		<div className='w-full flex flex-col gap-4'>
-			<div className='w-full flex flex-col md:flex-row gap-4 '>
+			<div className='w-full flex flex-col md:flex-row gap-4 md:px-24'>
 				<ImageContainer />
 				<ProductInfo />
 			</div>
