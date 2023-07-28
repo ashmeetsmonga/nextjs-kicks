c8ba37e109cfc5ddd7674ea539ea66cc9dd26730
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-28 16:18:26 +0530

    shadow added to menu

--- app/components/NavbarMenu.tsx
+++ app/components/NavbarMenu.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NavbarMenu = () => {
   return (
-    <div className="absolute left-10 top-28 z-10 flex flex-col items-start gap-4 rounded-lg bg-white p-4">
+    <div className="absolute left-10 top-28 z-10 flex flex-col items-start gap-4 rounded-lg bg-white p-4 shadow-2xl">
       <Link href="/menu/life-style-shoes">Life Style Shoes</Link>
       <Link href="/menu/basketball-shoes">Basketball Shoes</Link>
     </div>
